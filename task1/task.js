efb26c727aee447862a39829ba8199dcf3ea9d2b
BZsombor19 <bogoszsomborr@gmail.com>
2025-09-19 12:06:24 +0200

    task1meg

--- task1/task.js
+++ task1/task.js
@@ -1,3 +1,40 @@
 export function findByActorNameAndGenre(allMovies, allActors, actorName, genre) {
+    let actorId = null;
 
+    for (let i = 0; i < allActors.length; i++) {
+        if (allActors[i].name === actorName) {
+            actorId = allActors[i].id;
+            break;
+        }
+    }
+
+    if (actorId === null) {
+        return [];
+    }
+    let result = [];
+
+    for (let i = 0; i < allMovies.length; i++) {
+        const movie = allMovies[i];
+
+        let haveActor = false;
+        for (let j = 0; j < movie.actor_ids.length; j++) {
+            if (movie.actor_ids[j] === actorId) {
+                haveActor = true;
+                break;
+            }
+        }
+
+        let haveGenre = false;
+        for (let k = 0; k < movie.genres.length; k++) {
+            if (movie.genres[k] === genre) {
+                haveGenre = true;
+                break;
+            }
+        }
+
+        if (haveActor && haveGenre) {
+            result.push(movie.title);
+        }
+    }
+    return result;
 }
