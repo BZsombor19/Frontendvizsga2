c8f011c81245f256a955442681525b92fa82e30a
BZsombor19 <bogoszsomborr@gmail.com>
2025-09-19 12:07:27 +0200

    taskmeg1

--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,17 @@
 export function findUniqueGenres(genres) {
+    let result = [];
 
+    for (let i = 0; i < genres.length; i++) {
+        let movieGenres = genres[i];
+
+        for (let j = 0; j < movieGenres.length; j++) {
+            let genre = movieGenres[j];
+
+            if (!result.includes(genre)) {
+                result.push(genre);
+            }
+        }
+    }
+
+    return result;
 }
